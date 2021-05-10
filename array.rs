6c55761f02edd02118059191abc15847ac0d45ec
scr165 <45762266+scr165@users.noreply.github.com>
2021-05-10 04:59:01 +0000

    Add files via upload

---
+++ array.rs
@@ -0,0 +1,10 @@
+//this program is about the creating an array
+//array saves a stack
+fn main(){
+    let a:[usize;5]=[1,2,3,4,5];//assigning values to array
+    let b=['a','b','c'];
+    let c=[3;5];//setting the 5 values in array to [3,3,3,3,3]
+    println!("{}",a[0]);
+    println!("{}",b[1]);
+    println!("{}",c[3]);//printing any value in this array is 3
+}
