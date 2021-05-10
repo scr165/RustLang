424cc9c2896fd76b9db1483bb2926433481476c6
scr165 <45762266+scr165@users.noreply.github.com>
2021-05-10 04:30:25 +0000

    Add files via upload

---
+++ tuple.rs
@@ -0,0 +1,10 @@
+//tghhis program is about creating a tuple and descrtucturing a tuple
+fn main() {
+let tup = (500, 6.4, 1); //aasigning a tuple
+let a=tup.0; //assigning index to the tuple
+let b=tup.1;
+let c=tup.2;
+println!("the value of a is {}",a);//printing out the tuple's elements
+println!("the value of b is {}",b);
+println!("the value of c is {}",c);
+}
