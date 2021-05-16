d87d9e75f06ee2c2619243d0e21cd66aa0ac1df3
scr165 <45762266+scr165@users.noreply.github.com>
2021-05-16 05:35:31 +0000

    Add files via upload

---
+++ return_value.rs
@@ -0,0 +1,8 @@
+fn five() ->i32 { //stating the function and return type
+    5
+}
+
+fn main(){
+    let x=five(); //assigning the value of the function to the variable by calling
+    println!("the value of x is {}",x);
+}
